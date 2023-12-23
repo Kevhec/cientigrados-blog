b67118fe755ccbb4c045b5060f0cde80a94157b8
Kevhec <kevhec.dev@gmail.com>
2023-12-23 00:24:56 -0500

    [ADD] Env variables [UPDATE] GH Action file for env

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,20 @@
 import { defineConfig } from 'astro/config';
 
 import { sanityIntegration } from "@sanity/astro";
+import dotenv from 'dotenv';
+
+dotenv.config({
+  path: '.env'
+})
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kevhec.github.io',
   base: '/cientigrados-frontend',
   integrations: [sanityIntegration({
-    projectId: "a0z0f7e5",
-    dataset: "production",
-    apiVersion: "2023-12-23",
+    projectId: process.env.PUBLIC_SANITY_STUDIO_PROJECT_ID,
+    dataset: process.env.PUBLIC_SANITY_STUDIO_DATASET,
+    apiVersion: process.env.PUBLIC_SANITY_STUDIO_API_VERSION,
     useCdn: false,
   })]
 });
